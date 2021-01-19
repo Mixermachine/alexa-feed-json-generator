06d000648d96ff2dc276f94ce17232dbb21afbb2
Aaron <info>
2021-01-20 00:49:10 +0100

    Adds files bind to script

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:15-alpine
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 COPY package*.json ./
 
--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 sudo docker build -t alexa-feed-json-generator .
 sudo docker stop alexa-feed-json-generator
-sudo docker run -d -p 6565:6565 --restart always alexa-feed-json-generator
+sudo docker run -d -p 6565:6565 --restart always \
+--name alexa-feed-json-generator \
+--mount type=bind,source="$(pwd)"/files,target=/app/files \
+alexa-feed-json-generator
