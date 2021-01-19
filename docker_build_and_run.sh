5177286c28a9170cc637df4fbc9483946e9f47eb
Aaron <info>
2021-01-19 20:29:29 +0100

    Make server run detached and always restart

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo docker build -t alexa-feed-json-generator .
-sudo docker run -p 6565:6565 alexa-feed-json-generator
+sudo docker run -d -p 6565:6565 --restart always alexa-feed-json-generator
