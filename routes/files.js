9e3ad2640c33d50f81dedc81fcd83c78d0363f2a
Aaron <info>
2021-02-06 00:52:19 +0100

    Adds files api to new main/pre structure

--- routes/files.js
+++ routes/files.js
@@ -2,23 +2,28 @@
 // @flow
 
 const {FastifyRequest, FastifyReply} = require('fastify');
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
 
-const folder = path.join(__dirname, '../files/')
+const folder = path.join(__dirname, '../files/');
 const prefix = '/files';
 
-module.exports = fastify => {
-    fastify.get(prefix + '/:filename', {}, (request: FastifyRequest, response: FastifyReply) => {
-        const stream = fs.createReadStream(folder + request.params.filename)
+function process(type: string, request: FastifyRequest, response: FastifyReply) {
+    const stream = fs.createReadStream(folder + '/' + type + '/' + request.params.filename);
 
-        stream.on('open', function () {
-            // This just pipes the read stream to the response object (which goes to the client)
-            response.type('audio/mpeg').send(stream);
-        });
+    stream.on('open', function () {
+        // This just pipes the read stream to the response object (which goes to the client)
+        response.type('audio/mpeg').send(stream);
+    });
 
-        stream.on('error', function (err) {
-            response.code(404).send({msg: 'Not found'});
-        });
+    stream.on('error', function (err) {
+        response.code(404).send({msg: 'Not found'});
     });
 }
+
+module.exports = fastify => {
+    const postfix = '/:filename';
+
+    fastify.get(prefix + '/main' + postfix, {}, (request, response) => process('main', request, response));
+    fastify.get(prefix + '/pre' + postfix, {}, (request, response) => process('pre', request, response));
+};
