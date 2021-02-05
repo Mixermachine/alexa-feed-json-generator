fc2f5d7afc8dffa2c591322da01010f73b76146c
Aaron <info>
2021-02-06 00:24:26 +0100

    Adapts folder mount, adds auto folder creation

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 sudo docker build -t alexa-feed-json-generator .
 sudo docker stop alexa-feed-json-generator
+
+mkdir files_in_container
+mkdir files_in_container/main
+mkdir files_in_container/pre
 sudo docker run -d -p 6565:6565 --restart always \
 --name alexa-feed-json-generator \
---mount type=bind,source="$(pwd)"/files,target=/app/files \
+--mount type=bind,source="$(pwd)"/files_in_container,target=/app/files \
 alexa-feed-json-generator
